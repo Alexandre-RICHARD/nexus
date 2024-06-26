337bc6c7682ca2be17217b6601f151d95bf49cdc
Alexandre Richard <alexandre.richard.dev@gmail.com>
2024-06-26 21:28:52 +0200

    :arrow_up: Update dependencies and complete dictionary country-language

--- src/dictionnaries/languageToCountry.ts
+++ src/dictionnaries/languageToCountry.ts
@@ -5,13 +5,13 @@ import type { LanguageToCountryTypes } from "../types/language";
 export const languageToCountry: LanguageToCountryTypes = {
   [LanguageEnum.AFRIKAANS]: CountryEnum.SOUTH_AFRICA,
   [LanguageEnum.ALBANIAN]: CountryEnum.ALBANIA,
-  [LanguageEnum.AMHARIC]: CountryEnum.UNITED_STATES, //! TODO
+  [LanguageEnum.AMHARIC]: CountryEnum.ETHIOPIA,
   [LanguageEnum.ARABIC]: CountryEnum.SAUDI_ARABIA,
   [LanguageEnum.ARMENIAN]: CountryEnum.ARMENIA,
   [LanguageEnum.AZERBAIJANI]: CountryEnum.AZERBAIJAN,
-  [LanguageEnum.BAMBARA]: CountryEnum.UNITED_STATES, //! TODO
+  [LanguageEnum.BAMBARA]: CountryEnum.MALI,
   [LanguageEnum.BELARUSIAN]: CountryEnum.BELARUS,
-  [LanguageEnum.BENGALI]: CountryEnum.UNITED_STATES, //! TODO
+  [LanguageEnum.BENGALI]: CountryEnum.BANGLADESH,
   [LanguageEnum.BULGARIAN]: CountryEnum.BULGARIA,
   [LanguageEnum.CATALAN]: CountryEnum.CATALONIA,
   [LanguageEnum.CHINESE]: CountryEnum.CHINA,
@@ -20,57 +20,57 @@ export const languageToCountry: LanguageToCountryTypes = {
   [LanguageEnum.DANISH]: CountryEnum.DENMARK,
   [LanguageEnum.DUTCH]: CountryEnum.NETHERLANDS,
   [LanguageEnum.ENGLISH]: CountryEnum.UNITED_STATES,
-  [LanguageEnum.ESPERANTO]: CountryEnum.UNITED_STATES, //! TODO
+  [LanguageEnum.ESPERANTO]: CountryEnum.CHINA,
   [LanguageEnum.FILIPINO]: CountryEnum.PHILIPPINES,
   [LanguageEnum.FINNISH]: CountryEnum.FINLAND,
   [LanguageEnum.FRENCH]: CountryEnum.FRANCE,
   [LanguageEnum.GEORGIAN]: CountryEnum.GEORGIA,
   [LanguageEnum.GERMAN]: CountryEnum.GERMANY,
   [LanguageEnum.GREEK]: CountryEnum.GREECE,
-  [LanguageEnum.GUJARATI]: CountryEnum.UNITED_STATES, //! TODO
-  [LanguageEnum.HAUSA]: CountryEnum.UNITED_STATES, //! TODO
-  [LanguageEnum.HEBREW]: CountryEnum.UNITED_STATES, //! TODO
+  [LanguageEnum.GUJARATI]: CountryEnum.INDIA,
+  [LanguageEnum.HAUSA]: CountryEnum.NIGERIA,
+  [LanguageEnum.HEBREW]: CountryEnum.ISRAEL,
   [LanguageEnum.HINDI]: CountryEnum.INDIA,
   [LanguageEnum.HUNGARIAN]: CountryEnum.HUNGARY,
   [LanguageEnum.ICELANDIC]: CountryEnum.ICELAND,
-  [LanguageEnum.IGBO]: CountryEnum.UNITED_STATES, //! TODO
+  [LanguageEnum.IGBO]: CountryEnum.NIGERIA,
   [LanguageEnum.INDONESIAN]: CountryEnum.INDONESIA,
   [LanguageEnum.ITALIAN]: CountryEnum.ITALY,
   [LanguageEnum.JAPANESE]: CountryEnum.JAPAN,
-  [LanguageEnum.JAVANESE]: CountryEnum.UNITED_STATES, //! TODO
+  [LanguageEnum.JAVANESE]: CountryEnum.INDONESIA,
   [LanguageEnum.KAZAKH]: CountryEnum.KAZAKHSTAN,
-  [LanguageEnum.KHMER]: CountryEnum.UNITED_STATES, //! TODO
-  [LanguageEnum.KINYARWANDA]: CountryEnum.UNITED_STATES, //! TODO
+  [LanguageEnum.KHMER]: CountryEnum.CAMBODIA,
+  [LanguageEnum.KINYARWANDA]: CountryEnum.RWANDA,
   [LanguageEnum.KOREAN]: CountryEnum.SOUTH_KOREA,
-  [LanguageEnum.KURDISH]: CountryEnum.UNITED_STATES, //! TODO
-  [LanguageEnum.LINGALA]: CountryEnum.UNITED_STATES, //! TODO
+  [LanguageEnum.KURDISH]: CountryEnum.TURKEY,
+  [LanguageEnum.LINGALA]: CountryEnum.DEMOCRATIC_REPUBLIC_OF_CONGO,
   [LanguageEnum.LITHUANIAN]: CountryEnum.LITHUANIA,
   [LanguageEnum.MALAY]: CountryEnum.MALAYSIA,
-  [LanguageEnum.MARATHI]: CountryEnum.UNITED_STATES, //! TODO
+  [LanguageEnum.MARATHI]: CountryEnum.INDIA,
   [LanguageEnum.MONGOLIAN]: CountryEnum.MONGOLIA,
   [LanguageEnum.NEPALI]: CountryEnum.NEPAL,
   [LanguageEnum.NORWEGIAN]: CountryEnum.NORWAY,
-  [LanguageEnum.OROMO]: CountryEnum.UNITED_STATES, //! TODO
-  [LanguageEnum.PERSIAN]: CountryEnum.UNITED_STATES, //! TODO
+  [LanguageEnum.OROMO]: CountryEnum.ETHIOPIA,
+  [LanguageEnum.PERSIAN]: CountryEnum.IRAN,
   [LanguageEnum.POLISH]: CountryEnum.POLAND,
   [LanguageEnum.PORTUGUESE]: CountryEnum.PORTUGAL,
-  [LanguageEnum.PUNJABI]: CountryEnum.UNITED_STATES, //! TODO
+  [LanguageEnum.PUNJABI]: CountryEnum.PAKISTAN,
   [LanguageEnum.RUSSIAN]: CountryEnum.RUSSIA,
   [LanguageEnum.SERBIAN]: CountryEnum.SERBIA,
-  [LanguageEnum.SOMALI]: CountryEnum.UNITED_STATES, //! TODO
+  [LanguageEnum.SOMALI]: CountryEnum.SOMALIA,
   [LanguageEnum.SPANISH]: CountryEnum.SPAIN,
-  [LanguageEnum.SUNDANESE]: CountryEnum.UNITED_STATES, //! TODO
-  [LanguageEnum.SWAHILI]: CountryEnum.UNITED_STATES, //! TODO
+  [LanguageEnum.SUNDANESE]: CountryEnum.INDONESIA,
+  [LanguageEnum.SWAHILI]: CountryEnum.TANZANIA,
   [LanguageEnum.SWEDISH]: CountryEnum.SWEDEN,
-  [LanguageEnum.TAMIL]: CountryEnum.UNITED_STATES, //! TODO
-  [LanguageEnum.TELEGU]: CountryEnum.UNITED_STATES, //! TODO
+  [LanguageEnum.TAMIL]: CountryEnum.INDIA,
+  [LanguageEnum.TELEGU]: CountryEnum.INDIA,
   [LanguageEnum.THAI]: CountryEnum.THAILAND,
   [LanguageEnum.TURKISH]: CountryEnum.TURKEY,
   [LanguageEnum.TURKMEN]: CountryEnum.TURKMENISTAN,
   [LanguageEnum.UKRAINIAN]: CountryEnum.UKRAINE,
-  [LanguageEnum.URDU]: CountryEnum.UNITED_STATES, //! TODO
-  [LanguageEnum.UYGHUR]: CountryEnum.UNITED_STATES, //! TODO
+  [LanguageEnum.URDU]: CountryEnum.PAKISTAN,
+  [LanguageEnum.UYGHUR]: CountryEnum.CHINA,
   [LanguageEnum.UZBEK]: CountryEnum.UZBEKISTAN,
   [LanguageEnum.VIETNAMESE]: CountryEnum.VIETNAM,
-  [LanguageEnum.YORUBA]: CountryEnum.UNITED_STATES, //! TODO
+  [LanguageEnum.YORUBA]: CountryEnum.NIGERIA,
 };
